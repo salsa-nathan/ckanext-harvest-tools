20ccad0ed61778501b59561beee3dc13089d4a07
Mark Calvert <markcalvert84@gmail.com>
2021-10-28 15:01:22 -0600

    Added toolkit imports Added url_for for redirect instead of hardcoding the harvest source name Reformated code

--- ckanext/harvest_tools/blueprint.py
+++ ckanext/harvest_tools/blueprint.py
@@ -1,32 +1,39 @@
 import logging
 import ckan.plugins.toolkit as toolkit
-import ckan.model as model
 from flask import Blueprint
 
 from ckanext.harvest_tools.logic.action import get
 
+log = logging.getLogger(__name__)
+get_action = toolkit.get_action
+h = toolkit.h
+render = toolkit.render
+get_action = toolkit.get_action
+_ = toolkit._
+redirect_to = toolkit.redirect_to
+url_for = toolkit.url_for
+
 harvest_tools = Blueprint('harvest_tools', __name__)
 
+
 def harvest_table_report():
-        vars = {}
+    vars = {}
 
-        vars['tables'] = get.get_harvest_table_info()
+    vars['tables'] = get.get_harvest_table_info()
 
-        return toolkit.render('harvest_tools/harvest_table_report.html',
-                           extra_vars=vars)
+    return render('harvest_tools/harvest_table_report.html',
+                  extra_vars=vars)
 
 
-def abort_harvest_job( harvest_job_id):
+def abort_harvest_job(harvest_job_id):
     try:
-        toolkit.get_action('harvest_job_abort')(None, {"id": harvest_job_id})
-        toolkit.h.flash_success(_('Harvest job ID %s - aborted' % harvest_job_id))
+        result = get_action('harvest_job_abort')(None, {"id": harvest_job_id})
+        h.flash_success(_('Harvest job ID %s - aborted' % harvest_job_id))
     except Exception as e:
-        toolkit.h.flash_error(_('ERROR aborting harvest job ID %s' % harvest_job_id))
+        h.flash_error(_('ERROR aborting harvest job ID %s' % harvest_job_id))
 
-    toolkit.h.redirect_to('/harvest/data-directory/job/%s' % harvest_job_id)
+    return redirect_to(url_for('harvest_job_show', source=result.get('source_id') if result else None, id=harvest_job_id))
 
 
-harvest_tools.add_url_rule('/harvest/abort_job/<harvest_job_id>', view_func=abort_harvest_job)
+harvest_tools.add_url_rule('/harvest/abort_job/<harvest_job_id>', methods=['GET', 'POST'], view_func=abort_harvest_job)
 harvest_tools.add_url_rule('/harvest/table_report', view_func=harvest_table_report)
-
-
